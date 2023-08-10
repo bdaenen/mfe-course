7f9f7487cd556138555d5341a37e6bc690da5f4d
Benno Daenen <benno.daenen@cegeka.com>
2023-08-10 17:14:23 +0200

    Fixed public path.

--- packages/marketing/config/webpack.prd.js
+++ packages/marketing/config/webpack.prd.js
@@ -7,7 +7,7 @@ const prdConfig = {
     mode: 'production',
     output: {
         filename: '[name].[contenthash].js',
-        publicPath: '/marketing/latest'
+        publicPath: '/marketing/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
